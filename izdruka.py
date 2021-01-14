a73bbd37dea0875fbfa977a8f48779be27791aeb
AnnaGeizane <AG1511@s1vsk.lv>
2021-01-14 10:40:52 +0000

    virknes

---
+++ izdruka.py
@@ -0,0 +1,15 @@
+"""Var rakstīt komentārus
+vairākās
+rindās"""
+print("sveika pasaule!")
+print(2*2)
+print(2*2, 2*3,2*4, "pitons")
+print(f"ja saskaitīsim 5 ar 7, iegūsim {5+7}.") #kombinēta izdruka - teksts un aprēķins
+print("sveika,"+"pasaule!")
+print("Anna")
+print("Anna")
+print("Anna")
+print("Anna")
+print("Anna "*5)
+print(f"šogad būs {60*60*24*365} sekundes.")
+print(0.1 + 0.2 - 0.3)
---
+++ virknes.py
@@ -0,0 +1,25 @@
+"""
+vards=input("kā tevi sauc? ")
+print(f"tavs vārds ir {vards}.")
+
+gadi=int{input("cik tev ir gadu? ")}
+print(f"tev ir {gadi} gadi.")
+print(f"tavs dzimšanas gads ir {2021-gadi}.")
+"""
+T=int{input("ievadi temperatūru grādos celsija")}
+print(f"tie ir {T*9/5+32} grādi pēc fārenheita skalas")
+
+#strings {rakstzīmju virknes}
+print("sveiki")
+print('sveiki')
+print("sveika\ pasaule")#izdrukā divā rindās
+print("sveika, \tpasaule")#izdrukā ar atkāpi tab
+
+#sting garus- len()
+print(len("sveiki"))
+print(len("es esmu šeit"))# atstarpes arī skaitās
+
+# [sākums:beigas:solis]
+myString="sveika paaule"
+print(myString)
+print(myString[0])#izdrukā pirmo rakstzīmi
