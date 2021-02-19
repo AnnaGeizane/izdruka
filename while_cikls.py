65fc173d35c1503a1e3333691b20a7e9d2ea04a1
AnnaGeizane <AG1511@s1vsk.lv>
2021-02-19 13:46:05 +0000

    kaut ka nesaprotams

---
+++ while_cikls.py
@@ -0,0 +1,34 @@
+print("sveiki")
+""""
+while True:
+  print("sveiki")#bezgalīgi drukās
+""""
+i = 0 #tipisks cikla mainīgais
+while i<5:
+  print("sveiki")
+  i += 1 #i=i+1
+print("i tagad ir ", i)
+
+j=0
+while j<5:
+  print("sveiki, nr.", j)
+  j += 1
+
+while i>0:
+  print("skaitam atpakaļ:",i)
+  i -= 1
+
+#ar soli 2
+i=20
+while True:
+  if i > 26:
+    break
+  print("i ir ", i)
+  i += 2
+
+a= int(input("ievadi eglītes augstumu"))
+v=0
+while v < a :
+  print("*" * a)
+  v += 1
+
